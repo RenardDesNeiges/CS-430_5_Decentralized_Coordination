604acc48e1bf16cf810db0561849627b83310d4e
Titouan Renard <titouan.renard@gmail.com>
2020-11-21 12:52:57 +0100

    cleaned class structure, build shells as well

--- src/auction/bNr.sh
+++ src/auction/bNr.sh
@@ -1,2 +1,4 @@
 cd "/Users/renard/Documents/etudes/EPFLMA1/IntelligentAgents/exercises/CS-430_5_Decentralized_Coordination/src/auction"
-./build.sh; ./runSim.sh
+status=$?
+[ $status -eq 0 ] && echo "Running tests"; ./build.sh; ./runSim.sh || echo "No tests were run"; exit 1
+
--- src/auction/src/assignement/Constraint.java
+++
@@ -1,6 +0,0 @@
-package assignement;
-
-public abstract class Constraint {
-	
-	abstract boolean compatible(Solution solution);
-}
